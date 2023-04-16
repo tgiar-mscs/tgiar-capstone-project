31207e9becc6d945e9e2c47f85adb932e6594a53
Tylor <uk8527gy@Go.MinnState.edu>
2023-04-16 12:48:30 -0500

    week 14 progress

--- script.js
+++ script.js
@@ -35,3 +35,37 @@ document.addEventListener('keydown', function (e) {
     }
   }
 });
+
+// Modal content
+const content = document.getElementById('insert-steps');
+content.innerHTML = `<ul>
+<li>Step 1: Simply type in your said emergency and hit enter.</li>
+<li>Step 2: Read the step-by-step guide to advice you on what to do.</li>
+<li>Step 3: Stay calm and breath!</li>
+</ul>`;
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById('modalBtn');
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
